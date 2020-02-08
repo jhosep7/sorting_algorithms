5c37fe15f2a794032b83b290a1a012520f963fc0
jhosep7 <jodia.ms@outlook.com>
2020-02-08 12:54:47 -0500

    105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -6,10 +6,10 @@
  */
 void radix_sort(int *array, size_t size)
 {
-  int may = Maximo(array, size);
+	int may = Maximo(array, size);
 
-  for (int exp = 1; may/exp > 0; exp *= 10)
-  {conteo(array, size, exp); }
+	for (int exp = 1; may / exp > 0; exp *= 10)
+	{conteo(array, size, exp); }
 }
 
 /**
@@ -20,11 +20,12 @@ void radix_sort(int *array, size_t size)
  */
 int Maximo(int arr[], int n)
 {
-    int maximo = arr[0];
-    for (int i = 1; i < n; i++)
-        if (arr[i] > maximo)
-        {maximo = arr[i]; }
-    return maximo;
+	int maximo = arr[0];
+
+	for (int i = 1; i < n; i++)
+		if (arr[i] > maximo)
+		{maximo = arr[i]; }
+	return (maximo);
 }
 
 /**
@@ -33,25 +34,20 @@ int Maximo(int arr[], int n)
  * @n: size of array
  * @exp: exp
  */
-
 void conteo(int arr[], int n, int exp)
 {
-    int arrB[n];
-    int i, cuenta[10] = {0};
+	int arrB[n], i, cuenta[10] = {0};
 
-    for (i = 0; i < n; i++)
-    {cuenta[ (arr[i]/exp)%10 ]++; }
-
-    for (i = 1; i < 10; i++)
-    {cuenta[i] += cuenta[i - 1]; }
-
-    for (i = n - 1; i >= 0; i--)
-    {
-        arrB[cuenta[ (arr[i]/exp)%10 ] - 1] = arr[i];
-        cuenta[ (arr[i]/exp)%10 ]--;
-    }
-
-    for (i = 0; i < n; i++)
-    {arr[i] = arrB[i]; }
-    print_array(arr, n);
+	for (i = 0; i < n; i++)
+	{cuenta[(arr[i] / exp) % 10]++; }
+	for (i = 1; i < 10; i++)
+	{cuenta[i] += cuenta[i - 1]; }
+	for (i = n - 1; i >= 0; i--)
+	{
+		arrB[cuenta[(arr[i] / exp) % 10] - 1] = arr[i];
+		cuenta[(arr[i] / exp) % 10]--;
+	}
+	for (i = 0; i < n; i++)
+	{arr[i] = arrB[i]; }
+	print_array(arr, n);
 }
